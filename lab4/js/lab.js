a02efa6b5f4ba40fffcbf0b9d0f4b50a51a3d95c
shlserra <shlserra@ucsc.edu>
2023-04-25 03:45:24 -0700

    LAB 4 HTML CSS IMG JS

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissor
+// Play rock-paper-scissors with a computer using user input via text.
+// Sho Serrano <shlserra@ucsc.edu>
+// April 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print Instructions of the Game
+// Print Blank Line
+  // Loop
+    // Game Loop
+      // Print the Three Possible Hand Signs
+      // Print Blank Line
+        // Input Loop
+          // Recieve User Input
+          // Check User Input
+          // If User Input Meets Requirements End Loop
+        // Print User Inputed Hand Sign
+      // Random Generate a Number
+      // Use Number to Decide CPU's Hand Sign
+      // Print CPU Handsign
+      // Print Blank Line
+      // Check Who Won 
+      // If No One Won: Print "Tie - Choose Again", Print Blank Line, and Continue Game Loop
+      // If the User Won: Print "You Won!", Print Blank Line, and End Game Loop
+      // If the CPU Won: Print "You Lose!", Print Blank Line, and End Game Loop
+    // Print "Want to play again? (Y/N)"
+      // Input Loop
+        //Recieve User Input
+        //Check User Input
+        //If User Input Meets Requirements End Loop
+      // If Yes: Continue Loop
+      // If No: End Loop
